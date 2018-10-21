7969f705b80285d5ab8bc4f2cf07e716388a70e2
fathikar <you@example.com>
2018-10-21 23:53:56 +0700

    first commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+nama = "Amiroh Mujahidah"
+nama = "Digital Talent"
+print(nama)
+a = 3
+b = 3
+print(a+b)
+print(a*b)
+print(a**b)
+a = 5
+print(a, "mempunyai tipe", type(a))
